c76530e4c491a6cd23d1b28ff2abbd40977c7881
Oumaima Fisaoui <oufisaou@e1r13p5.1337.ma>
2022-12-20 21:10:32 +0100

    put walls inside the struct

--- move_player.c
+++ move_player.c
@@ -6,7 +6,7 @@
 /*   By: oufisaou <oufisaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 14:27:59 by oufisaou          #+#    #+#             */
-/*   Updated: 2022/12/20 21:04:17 by oufisaou         ###   ########.fr       */
+/*   Updated: 2022/12/20 21:09:02 by oufisaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,22 @@
 // 	return (1);
 // }
 
-// int	mouvements(int key, t_all *cub)
-// {
-// 	int	event;
+int	mouvements(int key, t_all *cub)
+{
+	int	event;
 
-// 	if (key == 13 || key == 1)
-// 		event = event_up_down(cub, key);
-// 	// else if (key == 2 || key == 0)
-// 	// 	event = event_left_right(cub, key);
-// 	// else if (key == 53)
-// 	// 	exit_program(cub);
-// 	//mlx_clear_window(cub->mlx, cub->mlx_win);
-// 	//add_texture(cub);
-// 	return (1);
-// }
+	if (key == 13 || key == 1)
+		event = event_up_down(cub, key);
+	// else if (key == 2 || key == 0)
+	// 	event = event_left_right(cub, key);
+	// else if (key == 53)
+	// 	exit_program(cub);
+	//mlx_clear_window(cub->mlx, cub->mlx_win);
+	//add_texture(cub);
+	return (1);
+}
 
-// void hook_player(t_all* cub)
-// {
-//     mlx_key_hook(cub->mlx_win, mouvements, &cub);
-// }
+void hook_player(t_all* cub)
+{
+    mlx_key_hook(cub->mlx_win, mouvements, &cub);
+}
